e38607f55b19b5d2cce2b2005fc25d76b52d6817
talitalayane <talitalayane@hotmail.com>
2022-01-24 20:00:08 -0300

    seeds feito

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+require "open-uri"
+
+
+puts "Criando conteúdo"
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath1 = 'netflix_titles.csv'
+
+CSV.foreach(filepath1, csv_options) do |row|
+  Netflix.create!(
+    title: row[2],
+    genre: row[1],
+    year: row[7],
+    country: row[5],
+    published_at: Date.parse(row[6]),
+    description: row[11]
+  )
+end
