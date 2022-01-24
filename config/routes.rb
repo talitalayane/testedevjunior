aabad4ae3e1cb6704d479e2141c9e839f720196c
talitalayane <talitalayane@hotmail.com>
2022-01-24 20:25:36 -0300

    rota de index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :netflixes, only: [:index]
+    end
+  end
 end
+ß
